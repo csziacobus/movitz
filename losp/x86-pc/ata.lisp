ceb8d32f2aee70f90cf478db9f0a4ee28923bada
Charles Zhang <charleszhang99@yahoo.com>
2014-12-01 20:40:15 -0500

    Fix indentation

--- losp/x86-pc/ata.lisp
+++ losp/x86-pc/ata.lisp
@@ -19,13 +19,13 @@
 (defpackage muerte.x86-pc.ata
   (:use muerte.cl muerte.lib muerte)
   (:export #:read-partition-table
-		   #:get-partition-data
-		   #:partition-active-p
-		   #:partition-type
-		   #:partition-start-offset
-		   #:partition-size
-		   #:lba-read-sector
-		   #:lba-write-sector))
+           #:get-partition-data
+           #:partition-active-p
+           #:partition-type
+           #:partition-start-offset
+           #:partition-size
+           #:lba-read-sector
+           #:lba-write-sector))
 
 (provide :x86-pc/ata)
 
@@ -54,9 +54,9 @@
 
 
 (defparameter *partitions* '((nil nil nil nil) ; controller 1 disc 1
-								 (nil nil nil nil) ; controller 1 disc 2
-								 (nil nil nil nil) ; controller 2 disc 1
-								 (nil nil nil nil))) ; controller 2 disc 2
+                             (nil nil nil nil) ; controller 1 disc 2
+                             (nil nil nil nil) ; controller 2 disc 1
+                             (nil nil nil nil))) ; controller 2 disc 2
 
 
 ;;;; --------------------------------------------------------------------------
@@ -65,43 +65,43 @@
 (defun busy-wait (controller)
   "ATA Driver busy spin loop."
   (loop while (logbitp +bitflag-busy+
-					   (io-port (+ controller
-								   +offset-status-command+)
-								:unsigned-byte8))))
+                       (io-port (+ controller
+                                   +offset-status-command+)
+                                :unsigned-byte8))))
 
 (defun send-command (controller command)
   "Send a command to an IDE controller."
   (setf (io-port (+ controller +offset-status-command+) :unsigned-byte8)
-		command))
+        command))
 
 (defun lba-read-write-common (controller drive-number block-address)
   (if (= 0 block-address)
-	  (error "Sector zero is not a valid index for LBA addressing."))
+      (error "Sector zero is not a valid index for LBA addressing."))
   ;; send a null
   (setf (io-port (+ controller
-					+offset-error-precomp+) :unsigned-byte8) 0)
+                    +offset-error-precomp+) :unsigned-byte8) 0)
   ;; send a sector count 
   (setf (io-port (+ controller
-					+offset-sector-count+):unsigned-byte8) 1)
+                    +offset-sector-count+):unsigned-byte8) 1)
   ;; send the lowest 8 bits of the lba
   (setf (io-port (+ controller +offset-lba1+) :unsigned-byte8)
-		(logand block-address #xFF))
+        (logand block-address #xFF))
   ;; send the next 8 bits
   (setf (io-port (+ controller +offset-lba2+) :unsigned-byte8)
-		(ash (logand block-address #xFF00)
-			 -8))
+        (ash (logand block-address #xFF00)
+             -8))
   ;; send the next 8 bits
   (setf (io-port (+ controller +offset-lba3+) :unsigned-byte8)
-		(ash (logand block-address #xFF0000)
-			 -16))
+        (ash (logand block-address #xFF0000)
+             -16))
   ;; send the last 4 bits and some magic bits
   (setf (io-port (+ controller +offset-drive-head+) :unsigned-byte8)
-		(logand
-		 (logior (ash (logand block-address #xF000000)
-					  -24)
-				 #xE0  ;; magic 
-				 (ash drive-number 4))
-		 #x0F)))
+        (logand
+         (logior (ash (logand block-address #xF000000)
+                      -24)
+                 #xE0  ;; magic 
+                 (ash drive-number 4))
+         #x0F)))
 
 
 (defun lba-read-sector (controller drive-number block-address)
@@ -114,10 +114,10 @@
   (busy-wait controller)
   ;; read the data
   (loop for position from 0 to 255
-	 for data = (io-port (+ controller +offset-data+)
-						   :unsigned-byte16)
-     collect (logand #x00FF data)
-	 collect (ash (logand #xFF00 data) -8)))
+        for data = (io-port (+ controller +offset-data+)
+                            :unsigned-byte16)
+        collect (logand #x00FF data)
+        collect (ash (logand #xFF00 data) -8)))
 
 
 (defun lba-write-sector (controller drive-number block-address data)
@@ -128,9 +128,9 @@
   (send-command controller +command-write+)
   (busy-wait controller)
   (loop for position from 0 to 511 by 2
-	 do (setf (io-port (+ controller +offset-data+) :unsigned-byte16)
-			  (+ (ash (nth position data) 8)
-				 (nth (+ 1 position) data)))))
+        do (setf (io-port (+ controller +offset-data+) :unsigned-byte16)
+                 (+ (ash (nth position data) 8)
+                    (nth (+ 1 position) data)))))
 
 
 ;;;; --------------------------------------------------------------------------
@@ -139,24 +139,24 @@
 (defun read-partition-table (controller drive-number)
   "Reads and stores the partition table from the specified disk."
   (cond ((> controller 1)			; assume 2 controllers for now
-		 (error "Invalid controller number.")))
+         (error "Invalid controller number.")))
   (cond ((> drive-number 1)				; assume 2 drives per controller for now
-		 (error "Invalid driver number.")))
+         (error "Invalid driver number.")))
 
   (let ((sector
-		  (cond ((= 0 controller)
-				 (lba-read-sector +controller0+ drive-number 1))
-				((= 1 controller)
-				 (lba-read-sector +controller1+ drive-number 1)))))
-	(setf (nth (+ controller drive-number) *partitions*)
-		  (list (loop for offset from 446 to 461
-				   collect (nth offset sector))
-				(loop for offset from 462 to 477
-				   collect (nth offset sector))
-				(loop for offset from 478 to 493
-				   collect (nth offset sector))
-				(loop for offset from 494 to 509
-				   collect (nth offset sector))))))
+          (cond ((= 0 controller)
+                 (lba-read-sector +controller0+ drive-number 1))
+                ((= 1 controller)
+                 (lba-read-sector +controller1+ drive-number 1)))))
+    (setf (nth (+ controller drive-number) *partitions*)
+          (list (loop for offset from 446 to 461
+                      collect (nth offset sector))
+                (loop for offset from 462 to 477
+                      collect (nth offset sector))
+                (loop for offset from 478 to 493
+                      collect (nth offset sector))
+                (loop for offset from 494 to 509
+                      collect (nth offset sector))))))
 
 (defun get-partition-data (controller drive-number partition)
   "Returns the partition data for the specified controller and drive from the local cache.
@@ -166,67 +166,54 @@
       partition-start-offset
       partition-size"
   (cond ((> controller 1)			; assume 2 controllers for now
-		 (error "Invalid controller number.")))
+         (error "Invalid controller number.")))
   (cond ((> drive-number 1)				; assume 2 drives per controller for now
-		 (error "Invalid driver number.")))
+         (error "Invalid driver number.")))
   (cond ((> partition 3)				; max 4 partitions per drive (no extended partitions for now)
-		 (error "Invalid partition number.")))
+         (error "Invalid partition number.")))
   (nth partition
-	   (nth (+ controller drive-number) *partitions*)))
+       (nth (+ controller drive-number) *partitions*)))
 
 (defun partition-active-p (partition-data)
   "Returns the state of the active flag for the partition."
   (cond ((= #x80 (nth 0 partition-data))
-		 t)
-		(t nil)))
+         t)
+        (t nil)))
 
 (defun partition-type (partition-data)
   "Returns the partition type for the partition."
   (let ((id (nth 4 partition-data)))
-	(cond ((= #x00 id)
-		   "EMPTY")
-		  ((= #x06 id)
-		   "FAT16")
-		  ((= #x07 id)
-		   "NTFS")
-		  ((= #x0b id)
-		   "FAT32")
-		  ((= #x0c id)
-		   "FAT32-LBA")
-		  ((= #x0e id)
-		   "FAT16-LBA")
-		  ((= #x82 id)
-		   "LINUX-SWAP")
-		  ((= #x83 id)
-		   "LINUX")
-		  ((= #xA5 id)
-		   "FREEBSD")
-		  ((= #xA6 id)
-		   "OPENBSD")
-		  ((= #xA9 id)
-		   "NETBSD")
-		  ((= #xE8 id)
-		   "LUKS")
-		  (t
-		   "UNKNOWN"))))
+    (case id
+      (#x00 "EMPTY")
+      (#x06 "FAT16")
+      (#x07 "NTFS")
+      (#x0b "FAT32")
+      (#x0c "FAT32-LBA")
+      (#x0e "FAT16-LBA")
+      (#x82 "LINUX-SWAP")
+      (#x83 "LINUX")
+      (#xA5 "FREEBSD")
+      (#xA6 "OPENBSD")
+      (#xA9 "NETBSD")
+      (#xE8 "LUKS")
+      (otherwise "UNKNOWN"))))
 
 (defun partition-start-offset (partition-data)
   "Returns the starting offset (LBA) for the partition."
   (+ (nth 8 partition-data)
-	 (ash (nth 9 partition-data)
-		  8)
-	 (ash (nth 10 partition-data)
-		  16)
-	 (ash (nth 11 partition-data)
-		  24)))
+     (ash (nth 9 partition-data)
+          8)
+     (ash (nth 10 partition-data)
+          16)
+     (ash (nth 11 partition-data)
+          24)))
 
 (defun partition-size (partition-data)
   "Returns the size of the partition."
   (+ (nth 12 partition-data)
-	 (ash (nth 13 partition-data)
-		  8)
-	 (ash (nth 14 partition-data)
-		  16)
-	 (ash (nth 15 partition-data)
-		  24)))
-
+     (ash (nth 13 partition-data)
+          8)
+     (ash (nth 14 partition-data)
+          16)
+     (ash (nth 15 partition-data)
+          24)))
